95c005b4bc9c2bf6d07b895b53ad16a074cd18ab
Holger Rapp <sirver@h566336>
2009-02-25 19:41:03 +0100

    added gzip middleware, added 404 and 500 templates

--- settings.py
+++ settings.py
@@ -58,6 +58,7 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware', # Remove this, when load gets to high
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
