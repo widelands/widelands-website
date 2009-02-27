a6085d5d4995975256d97787574b8dfa6e2905d2
Holger Rapp <sirver@Kallisto.local>
2009-02-27 14:07:17 +0100

    Changed markdown library to markdown2

---
+++ mainpage/templatetags/wl_markdown.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python -tt
+# encoding: utf-8
+#
+# File: mainpage/templatetags/wl_markdown.py
+#
+# Created by Holger Rapp on 2009-02-27.
+# Copyright (c) 2009 HolgerRapp@gmx.net. All rights reserved.
+#
+# Last Modified: $Date$
+#
+
+from django import template
+from django.conf import settings
+from django.utils.encoding import smart_str, force_unicode
+from django.utils.safestring import mark_safe
+import markdown2
+
+register = template.Library()
+
+@register.filter
+def markdown(value, arg=''):
+    """
+    My own markup filter, wrapping the markup2 library, which is less bugged.
+    """
+    return mark_safe(force_unicode(markdown2.markdown(smart_str(value))))
+markdown.is_safe = True
+
