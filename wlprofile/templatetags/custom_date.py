2743b9a82d7f23434be92df0747ee0959f7d8b68
Holger Rapp <HolgerRapp@gmx.net>
2009-03-16 18:28:02 +0100

    Added custom date format

---
+++ wlprofile/templatetags/custom_date.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python -tt
+# encoding: utf-8
+#
+# File: wlprofile/templatetags/wlprofile.py
+#
+# Created by Holger Rapp on 2009-03-15.
+# Copyright (c) 2009 HolgerRapp@gmx.net. All rights reserved.
+#
+# Last Modified: $Date$
+#
+
+from django.utils.translation import ugettext as _
+from django import template
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
+from django.template.defaultfilters import date as django_date
+
+import re
+import datetime
+
+register = template.Library()
+
+
+natural_day_expr =  re.compile(r'''\%ND\((.*?)\)''')
+natural_year_expr =  re.compile(r'''\%NY\((.*?)\)''')
+
+def do_custom_date( format, date, now= None ):
+    """
+    Returns a string formatted representation of date according to format. This accepts
+    all formats that strftime also accepts, but it also accepts some new options which are dependant
+    on the current date.
+        %NY(format)        (natural year) for example %NY(.%Y) will give ".2008" if this year is not 2008, else 
+                            an empty string
+        %ND(alternatives)  (natural day) for example %ND(%d.%m.%Y) 
+        -> Yields today, yesterday, tomorrow or 2.12.2008
+    
+    format - format string as described above
+    date   - datetime object to display
+    now    - overwrite the value for now; only for debug reasons 
+    """
+    if now is None:
+        now = datetime.datetime.now()
+        
+    def _replace_ny(g):
+        if now.year == date.year:
+            return ""
+        return g.group(1)
+    
+    def _replace_nd(g):
+        delta = datetime.date(date.year,date.month,date.day) - \
+                datetime.date(now.year,now.month,now.day)
+        if delta.days == 0:
+            return _(ur'\t\o\d\a\y')
+        elif delta.days == 1:
+            return _(ur'\t\o\m\o\r\r\o\w')
+        elif delta.days == -1:
+            return _(ur'\y\e\s\t\e\r\d\a\y')
+        else:
+            return g.group(1)
+    try:
+        while 1:
+            oformat = format
+            format = natural_year_expr.sub(_replace_ny,format)
+            format = natural_day_expr.sub(_replace_nd,format)
+            if oformat == format:
+                break
+        
+        data = django_date(date,format)
+    except NotImplementedError:
+        return format
+
+    return data
+
+@register.filter
+def custom_date( date, user ):
+    """
+    If this user is logged in, return his representation, 
+    otherwise, return a sane default
+    """
+    if user.is_anonymous():
+        return django_date("j F Y")
+    return do_custom_date( user.get_profile().time_display, date )
+custom_date.is_safe = False
--- wlprofile/tests.py
+++ wlprofile/tests.py
@@ -1,23 +1,82 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+#!/usr/bin/python -tt
 
-Replace these with more appropriate tests for your application.
-"""
+import unittest
+import datetime
 
-from django.test import TestCase
+from templatetags.custom_date import do_custom_date
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class _CustomDate_Base( unittest.TestCase ):
+    def setUp(self):
+        self.date = datetime.datetime( 2008, 4, 12, 12, 53, 21 )
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class TestCustomDate_PythonReplacement_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        rv = do_custom_date("r",self.date)
+        self.assertEqual("Sat, 12 Apr 2008 12:53:21 +0200", rv)
+class TestCustomDate_PythonReplacement2_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        rv = do_custom_date("j.m.Y",self.date)
+        self.assertEqual("12.04.2008", rv)
+class TestCustomDate_NaturalYearReplacementSame_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2008, 4, 12 , 12, 53, 21 )
+        rv = do_custom_date("m%NY(.Y)",self.date, now)
+        self.assertEqual("04", rv)
+class TestCustomDate_NaturalYearReplacementDifferent_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2009, 4, 12 , 12, 53, 21 )
+        rv = do_custom_date("m%NY(.Y)",self.date, now)
+        self.assertEqual("04.2008", rv)
+class TestCustomDate_NaturalYearReplacementTwice_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2009, 4, 12 , 12, 53, 21 )
+        rv = do_custom_date(r"m%NY(.Y) \b\l\a\h \m\o\r\e %NY(m.Y)",self.date, now)
+        self.assertEqual("04.2008 blah more 04.2008", rv)
+class TestCustomDate_NaturalDayReplacementToday_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2008, 4, 12 , 0, 0, 21 )
+        rv = do_custom_date("j.m.y: %ND(j.m.y)",self.date, now)
+        self.assertEqual("12.04.08: today", rv)
+class TestCustomDate_NaturalDayReplacementTomorrow_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2008, 4, 11 , 23, 59, 59 )
+        rv = do_custom_date("j.m.y: %ND(j.m.y)",self.date, now)
+        self.assertEqual("12.04.08: tomorrow", rv)
+class TestCustomDate_NaturalDayReplacementYesterday_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2008, 4, 13 , 00, 00, 01 )
+        rv = do_custom_date("j.m.y: %ND(j.m.y)",self.date, now)
+        self.assertEqual("12.04.08: yesterday", rv)
+class TestCustomDate_NaturalDayReplacementNoSpecialDay_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2011, 4, 13 , 00, 00, 01 )
+        rv = do_custom_date("j.m.y: %ND(j.m.Y)",self.date, now)
+        self.assertEqual("12.04.08: 12.04.2008", rv)
+class TestCustomDate_RecursiveReplacementNoHit_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2011, 4, 13 , 00, 00, 01 )
+        rv = do_custom_date("j.m.y%ND(: j.m%NY(.Y))",self.date, now)
+        self.assertEqual("12.04.08: 12.04.2008", rv)
+class TestCustomDate_RecursiveReplacementMissDayHitYear_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2008, 9, 13 , 00, 00, 01 )
+        rv = do_custom_date("j.m.y: %ND(j.m.%NY(Y))",self.date, now)
+        self.assertEqual("12.04.08: 12.04.", rv)
+class TestCustomDate_RecursiveReplacementHitDayTodayHitYear_ExceptCorrectResult(_CustomDate_Base):
+    def runTest(self):
+        now = datetime.datetime( 2008, 4, 12 , 00, 00, 01 )
+        rv = do_custom_date("j.m.y: %ND(j.m.%NY(Y))",self.date, now)
+        self.assertEqual("12.04.08: today", rv)
 
->>> 1 + 1 == 2
-True
-"""}
 
+#########
+# FAILS #
+#########
+class TestCustomDate_FaultyDate_ExceptNoop(unittest.TestCase):
+    def runTest(self):
+        rv = do_custom_date("%c",(93,93))
+        self.assertEqual("%c", rv)
+
+if __name__ == '__main__':
+    unittest.main()
+    
