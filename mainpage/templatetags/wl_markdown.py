c0d1f2387bad669e72a2472547327094b1a12211
Holger Rapp <SirVer@gmx.de>
2010-03-14 14:12:22 +0100

    Removed SVN_URL

--- mainpage/templatetags/wl_markdown.py
+++ mainpage/templatetags/wl_markdown.py
@@ -35,7 +35,7 @@ except ImportError:
 # We will also need the site domain
 from django.contrib.sites.models import Site
 from settings import SITE_ID, SMILEYS, SMILEY_DIR, \
-        SMILEY_PREESCAPING, SVN_URL, BZR_URL
+        SMILEY_PREESCAPING, BZR_URL
 
 try:
     _domain = Site.objects.get(pk=SITE_ID).domain
