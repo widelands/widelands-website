ada90f2d9eb0553896f5d3b57c86664b18e4264d
Holger Rapp <SirVer@gmx.de>
2010-09-26 15:30:30 +0200

    Added a simple events model to display dates on the homepage

---
+++ wlevents/admin.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python -tt
+# encoding: utf-8
+
+from models import Event
+from django.contrib import admin
+
+class EventAdmin(admin.ModelAdmin):
+    search_fields = [ "name" ]
+    list_display = ["name", "start_date"]
+    list_filter = [ "start_date" ]
+
+admin.site.register(Event, EventAdmin)
+
