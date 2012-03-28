16a73ad0ef2ecff9f2f1e6eb2ee10fe80d727eca
Shevonar <infomh@anmaruco.de>
2012-03-29 01:33:34 +0200

    Added mainpage templates with images, css, js for the new design

---
+++ media/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+	$('[placeholder]').focus(function() {
+		var input = $(this);
+		if (input.val() == input.attr('placeholder')) {
+			input.val('');
+			input.removeClass('placeholder');
+		}
+	}).blur(function() {
+	var input = $(this);
+		if (input.val() == '' || input.val() == input.attr('placeholder')) {
+			input.addClass('placeholder');
+			input.val(input.attr('placeholder'));
+		}
+	}).blur();
+});
