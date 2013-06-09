9c61756008fb9fc363478a386701d1bdc5bac4ee
Holger Rapp <sirver@widelands.org>
2013-06-09 17:20:32 +0200

    Changes to make website work with gunicorn in an easy fashion.

---
+++ wlwebsite_wsgi.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+parent_dir = lambda dir: os.path.abspath(os.path.join(dir, os.pardir))
+
+code_directory = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+
+current_dir = code_directory
+activate_this = None
+while current_dir != "/":
+   current_dir = parent_dir(current_dir)
+   if os.path.exists(os.path.join(current_dir, "bin", "activate_this.py")):
+      activate_this = os.path.join(current_dir, "bin", "activate_this.py")
+      break
+
+if activate_this is None:
+   raise RuntimeException("Could not find virtualenv to start up!")
+
+execfile(activate_this, dict(__file__=activate_this))
+
+sys.path.append(parent_dir(code_directory))
+sys.path.append(code_directory)
+sys.path.append(os.path.join(code_directory, "widelands"))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'widelands.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
