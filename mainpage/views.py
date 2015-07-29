cc1be5832066f2f13d52564402a16ed5b2a55167
franku <somal@arcor.de>
2015-07-29 18:43:51 +0200

    Code after Pyformat

--- mainpage/views.py
+++ mainpage/views.py
@@ -8,9 +8,10 @@ import json
 import os
 import os.path
 
+
 def mainpage(request):
     return render_to_response('mainpage.html',
-                context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
 
 
 from forms import RegistrationWithCaptchaForm
@@ -19,111 +20,118 @@ from django.core.urlresolvers import reverse
 
 from registration.backends.default import DefaultBackend
 
+
 def register(request):
-    """
-    Overwritten view from registration to include a captcha.
-    We only need this because the remote IP addr must be passed
-    to the form; the registration doesn't do this
+    """Overwritten view from registration to include a captcha.
+
+    We only need this because the remote IP addr must be passed to the
+    form; the registration doesn't do this
+
     """
     remote_ip = request.META['REMOTE_ADDR']
     if request.method == 'POST':
-        form = RegistrationWithCaptchaForm(remote_ip,data=request.POST,
-                            files=request.FILES)
+        form = RegistrationWithCaptchaForm(remote_ip, data=request.POST,
+                                           files=request.FILES)
         if form.is_valid():
             new_user = DefaultBackend().register(request, **form.cleaned_data)
             return HttpResponseRedirect(reverse('registration_complete'))
     else:
         form = RegistrationWithCaptchaForm(remote_ip)
 
-    return render_to_response("registration/registration_form.html",
-                              { 'registration_form': form },
+    return render_to_response('registration/registration_form.html',
+                              {'registration_form': form},
                               context_instance=RequestContext(request))
 
+
 def developers(request):
+    """This reads out some json files in the SVN directory, and returns it as a
+    wl_markdown_object.
+
+    This replaces the wiki developers list
+
     """
-    This reads out some json files in the SVN directory, and returns it
-    as a wl_markdown_object. This replaces the wiki developers list
-    """
-    
-    # Get locale and translator names from each .json file and 
+
+    # Get locale and translator names from each .json file and
     # store them in one list.
-    txt = ""
+    txt = ''
     transl_files = []
     transl_list = []
-    path = os.path.normpath(WIDELANDS_SVN_DIR + "i18n/locales/")
+    path = os.path.normpath(WIDELANDS_SVN_DIR + 'i18n/locales/')
     try:
         transl_files = os.listdir(path)
         if transl_files:
             for fname in transl_files:
-                if fname.endswith(".json"):
-                    with open(path + "/" + fname,"r") as f:
+                if fname.endswith('.json'):
+                    with open(path + '/' + fname, 'r') as f:
                         json_data = json.load(f)
                     try:
-                        if json_data['translator-list'] != "translator-credits":
+                        if json_data['translator-list'] != 'translator-credits':
                             if not 'your-language-name-in-english' in json_data:
-                                transl_list = ["KeyError"]
+                                transl_list = ['KeyError']
                                 break
                             transl_list.append(json_data)
                     except KeyError:
-                        transl_list = ["KeyError"]
+                        transl_list = ['KeyError']
                         break
-                        
+
             # No KeyError -> Sort the list
             if 'KeyError' in transl_list:
-                txt = "Some Translator key is wrong, please contact the Developers.\n"
+                txt = 'Some Translator key is wrong, please contact the Developers.\n'
             else:
-                transl_list.sort(key=itemgetter('your-language-name-in-english'))
+                transl_list.sort(key=itemgetter(
+                    'your-language-name-in-english'))
 
         else:
-            txt = "No files for translators found!\n"
+            txt = 'No files for translators found!\n'
     except OSError:
         txt = txt + "Couldn't find translators directory!\n"
 
-               
     # Get other developers, put in the translators list
     # at given position and prepare all for wl_markdown
     try:
-        with open(WIDELANDS_SVN_DIR + "txts/developers.json", "r") as f:
+        with open(WIDELANDS_SVN_DIR + 'txts/developers.json', 'r') as f:
             json_data = json.load(f)['developers']
 
         for head in json_data:
             # Add first header
-            txt = txt + "##" + head['heading'] + "\n"
+            txt = txt + '##' + head['heading'] + '\n'
             # Inserting Translators if there was no error
             if head['heading'] == 'Translators' and 'KeyError' not in transl_list:
                 for values in (transl_list):
                     # Add subheader for locale
-                    txt = txt + "### " + values["your-language-name-in-english"] + "\n"
+                    txt = txt + '### ' + \
+                        values['your-language-name-in-english'] + '\n'
                     # Prepaire the names for wl_markdown
-                    txt = txt + "* " + values['translator-list'].replace('\n', '\n* ') + "\n"
-                    
+                    txt = txt + '* ' + \
+                        values['translator-list'].replace('\n', '\n* ') + '\n'
+
             # Add a subheader or/and the member(s)
             for entry in head['entries']:
                 if 'subheading' in entry.keys():
-                    txt = txt + "###" + entry['subheading'] + "\n"
+                    txt = txt + '###' + entry['subheading'] + '\n'
                 if 'members' in entry.keys():
                     for name in entry['members']:
-                        txt = txt + "* " + name + "\n"
+                        txt = txt + '* ' + name + '\n'
     except IOError:
         txt = txt + "Couldn't find developer file!"
-                    
-    txt = do_wl_markdown(txt,custom=False)
 
-    return render_to_response("mainpage/developers.html",
-                              {"developers": txt},
+    txt = do_wl_markdown(txt, custom=False)
+
+    return render_to_response('mainpage/developers.html',
+                              {'developers': txt},
                               context_instance=RequestContext(request)
-    )
-
+                              )
 
 
 def changelog(request):
-    """
-    This reads out the changelog in the SVN directory, and returns it
-    as a wl_markdown_object. This replaces the wiki changelog
-    """
-    data = open(WIDELANDS_SVN_DIR + "ChangeLog", "r").read()
-    return render_to_response("mainpage/changelog.html",
-                              {"changelog": data},
-                              context_instance=RequestContext(request)
-    )
+    """This reads out the changelog in the SVN directory, and returns it as a
+    wl_markdown_object.
 
+    This replaces the wiki changelog
+
+    """
+    data = open(WIDELANDS_SVN_DIR + 'ChangeLog', 'r').read()
+    return render_to_response('mainpage/changelog.html',
+                              {'changelog': data},
+                              context_instance=RequestContext(request)
+                              )
