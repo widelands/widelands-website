906e2d636a600699eb13eb491bb69ac0c27e7c73
GunChleoc <fios@foramnagaidhlig.net>
2016-05-22 09:30:55 +0100

    Ran pyformat.

--- wlhelp/management/commands/update_help.py
+++ wlhelp/management/commands/update_help.py
@@ -29,20 +29,25 @@ from settings import MEDIA_ROOT, WIDELANDS_SVN_DIR, MEDIA_URL
 from widelandslib.tribe import *
 from widelandslib.make_flow_diagram import make_all_subgraphs
 
+
 class TribeParser(object):
-    map_mouseover_pattern = re.compile(r'(?P<beginning>.*href="../../(?P<type>[^/]+)s/(?P<name>[^/]+)/".*")&lt;TABLE&gt;(?P<rest>.*)')
+    map_mouseover_pattern = re.compile(
+        r'(?P<beginning>.*href="../../(?P<type>[^/]+)s/(?P<name>[^/]+)/".*")&lt;TABLE&gt;(?P<rest>.*)')
+
     def __init__(self, name):
-        """
-        Parses the definitions for one tribe and generates the models
+        """Parses the definitions for one tribe and generates the models.
 
         name - name of the tribe
-        """
-        self._delete_old_media_dir(name) # You can deactivate this line if you don't need to clean house.
 
-        base_directory = os.path.normpath(WIDELANDS_SVN_DIR + "/data")
-        json_directory = os.path.normpath(MEDIA_ROOT + "/map_object_info")
-        
-        tribeinfo_file = open(os.path.normpath(json_directory + "/tribe_" + name + ".json"), "r")
+        """
+        self._delete_old_media_dir(
+            name)  # You can deactivate this line if you don't need to clean house.
+
+        base_directory = os.path.normpath(WIDELANDS_SVN_DIR + '/data')
+        json_directory = os.path.normpath(MEDIA_ROOT + '/map_object_info')
+
+        tribeinfo_file = open(os.path.normpath(
+            json_directory + '/tribe_' + name + '.json'), 'r')
         tribeinfo = json.load(tribeinfo_file)
 
         self._tribe = Tribe(tribeinfo, json_directory)
@@ -51,101 +56,111 @@ class TribeParser(object):
         self._to.displayname = tribeinfo['descname']
         self._to.descr = tribeinfo['tooltip']
         # copy icon
-        dn = "%s/wlhelp/img/%s/" % (MEDIA_ROOT, tribeinfo['name'])
+        dn = '%s/wlhelp/img/%s/' % (MEDIA_ROOT, tribeinfo['name'])
         try:
             os.makedirs(dn)
         except OSError, o:
             if o.errno != 17:
                 raise
-        new_name = path.join(dn, "icon.png")
-        file = os.path.normpath(base_directory + "/" + tribeinfo['icon'])
-        shutil.copy(file, new_name )
-        self._to.icon_url = path.normpath("%s/%s" % (MEDIA_URL, new_name[len(MEDIA_ROOT):]))
+        new_name = path.join(dn, 'icon.png')
+        file = os.path.normpath(base_directory + '/' + tribeinfo['icon'])
+        shutil.copy(file, new_name)
+        self._to.icon_url = path.normpath(
+            '%s/%s' % (MEDIA_URL, new_name[len(MEDIA_ROOT):]))
         self._to.save()
 
-    def parse( self, tribename, base_directory, json_directory ):
-        """Put all data into the database"""
-        self._delete_old_data(tribename) # You can deactivate this line if you don't need to clean house.
+    def parse(self, tribename, base_directory, json_directory):
+        """Put all data into the database."""
+        self._delete_old_data(
+            tribename)  # You can deactivate this line if you don't need to clean house.
 
-        wares_file = open(os.path.normpath(json_directory + "/" + tribename + "_wares.json"), "r")
+        wares_file = open(os.path.normpath(
+            json_directory + '/' + tribename + '_wares.json'), 'r')
         self._parse_wares(base_directory, json.load(wares_file))
 
-        workers_file = open(os.path.normpath(json_directory + "/" + tribename + "_workers.json"), "r")
+        workers_file = open(os.path.normpath(
+            json_directory + '/' + tribename + '_workers.json'), 'r')
         self._parse_workers(base_directory, json.load(workers_file))
 
-        buildings_file = open(os.path.normpath(json_directory + "/" + tribename + "_buildings.json"), "r")
+        buildings_file = open(os.path.normpath(
+            json_directory + '/' + tribename + '_buildings.json'), 'r')
         self._parse_buildings(base_directory, json.load(buildings_file))
 
-    def graph( self ):
-        """Make all graphs"""
+    def graph(self):
+        """Make all graphs."""
         tdir = make_all_subgraphs(self._tribe)
-        for obj, cls in [(WorkerModel, "workers"),
-                         (BuildingModel, "buildings"),
-                         (WareModel, "wares")]:
+        for obj, cls in [(WorkerModel, 'workers'),
+                         (BuildingModel, 'buildings'),
+                         (WareModel, 'wares')]:
             for inst in obj.objects.all().filter(tribe=self._to):
                 try:
-                    fpath = path.join(tdir,"help/%s/%s/%s/" % (self._tribe.name, cls, inst.name))
-                    url = self._copy_picture(path.join(fpath, "menu.png"), inst.name, "graph.png")
+                    fpath = path.join(tdir, 'help/%s/%s/%s/' %
+                                      (self._tribe.name, cls, inst.name))
+                    url = self._copy_picture(
+                        path.join(fpath, 'menu.png'), inst.name, 'graph.png')
                     inst.graph_url = url
-                    inst.imagemap = open(path.join(fpath, "map.map")).read()
-                    inst.imagemap = self.map_mouseover_pattern.sub(r"\1Show the \2 \3\4", inst.imagemap)
+                    inst.imagemap = open(path.join(fpath, 'map.map')).read()
+                    inst.imagemap = self.map_mouseover_pattern.sub(
+                        r"\1Show the \2 \3\4", inst.imagemap)
                     inst.save()
                 except Exception, e:
-                    print "Exception while handling", cls, "of", self._tribe.name, ":", inst.name
+                    print 'Exception while handling', cls, 'of', self._tribe.name, ':', inst.name
                     print type(e), e, repr(e)
 
         shutil.rmtree(tdir)
 
     def _delete_old_media_dir(self, tribename):
-		 """Clean house, e.g. when we have renamed a map object"""
+        """Clean house, e.g. when we have renamed a map object."""
 
-		 print("Deleting old media files...");
-		 sdir = os.path.join(MEDIA_ROOT, "wlhelp/img", tribename)
-		 if os.path.exists(sdir):
-			 shutil.rmtree(sdir)
+        print('Deleting old media files...')
+        sdir = os.path.join(MEDIA_ROOT, 'wlhelp/img', tribename)
+        if os.path.exists(sdir):
+            shutil.rmtree(sdir)
 
     def _delete_old_data(self, tribename):
-		 """Clean house, e.g. when we have renamed a map object"""
+        """Clean house, e.g. when we have renamed a map object."""
 
-		 t = TribeModel.objects.get(name=tribename)
-		 print("Deleting old wares...");
-		 for ware in WareModel.objects.filter(tribe=t):
-			ware.delete()
-		 print("Deleting old workers...");
-		 for worker in WorkerModel.objects.filter(tribe=t):
-			worker.delete()
-		 print("Deleting old buildings...");
-		 for building in BuildingModel.objects.filter(tribe=t):
-			building.delete()
+        t = TribeModel.objects.get(name=tribename)
+        print('Deleting old wares...')
+        for ware in WareModel.objects.filter(tribe=t):
+            ware.delete()
+        print('Deleting old workers...')
+        for worker in WorkerModel.objects.filter(tribe=t):
+            worker.delete()
+        print('Deleting old buildings...')
+        for building in BuildingModel.objects.filter(tribe=t):
+            building.delete()
 
-    def _copy_picture( self, file, name, fname ):
-        """
-        Copy the given image into the media directory
+    def _copy_picture(self, file, name, fname):
+        """Copy the given image into the media directory.
 
         file            - original path of image
         name            - name of the item (coal, iron...)
         fname           - file name of the picture
+
         """
-        dn = "%s/wlhelp/img/%s/%s/" % (MEDIA_ROOT,self._to.name,name)
+        dn = '%s/wlhelp/img/%s/%s/' % (MEDIA_ROOT, self._to.name, name)
         try:
             os.makedirs(dn)
         except OSError, o:
             if o.errno != 17:
                 raise
         new_name = path.join(dn, fname)
-        shutil.copy(file, new_name )
+        shutil.copy(file, new_name)
 
-        return "%s/%s" % (MEDIA_URL, new_name[len(MEDIA_ROOT):])
+        return '%s/%s' % (MEDIA_URL, new_name[len(MEDIA_ROOT):])
 
-    def _parse_workers( self, base_directory, workersinfo ):
-        """Put the workers into the database"""
-        print "  parsing workers"
+    def _parse_workers(self, base_directory, workersinfo):
+        """Put the workers into the database."""
+        print '  parsing workers'
 
         for worker in workersinfo['workers']:
-            print "    " + worker['name']
-            nn = self._copy_picture(os.path.normpath(base_directory + "/" + worker['icon']), worker['name'], "menu.png")
+            print '    ' + worker['name']
+            nn = self._copy_picture(os.path.normpath(
+                base_directory + '/' + worker['icon']), worker['name'], 'menu.png')
 
-            workero = WorkerModel.objects.get_or_create( tribe = self._to, name = worker['name'] )[0]
+            workero = WorkerModel.objects.get_or_create(
+                tribe=self._to, name=worker['name'])[0]
             workero.displayname = worker['descname']
             workero.image_url = nn
 
@@ -161,20 +176,22 @@ class TribeParser(object):
                 try:
                     enname = worker.becomes
                     workero.becomes = WorkerModel.objects.get_or_create(
-                            name=worker['becomes']['name'], tribe = self._to)[0]
+                        name=worker['becomes']['name'], tribe=self._to)[0]
                 except:
                     pass
 
             workero.save()
 
-    def _parse_wares( self, base_directory, waresinfo ):
-        print "  parsing wares"
+    def _parse_wares(self, base_directory, waresinfo):
+        print '  parsing wares'
 
         for ware in waresinfo['wares']:
-            print "    " + ware['name']
-            nn = self._copy_picture(os.path.normpath(base_directory + "/" + ware['icon']), ware['name'], "menu.png")
+            print '    ' + ware['name']
+            nn = self._copy_picture(os.path.normpath(
+                base_directory + '/' + ware['icon']), ware['name'], 'menu.png')
 
-            w = WareModel.objects.get_or_create( tribe = self._to, name = ware['name'] )[0]
+            w = WareModel.objects.get_or_create(
+                tribe=self._to, name=ware['name'])[0]
             w.displayname = ware['descname']
             w.image_url = nn
 
@@ -183,110 +200,126 @@ class TribeParser(object):
 
             w.save()
 
-    def _parse_buildings( self, base_directory, buildingsinfo ):
+    def _parse_buildings(self, base_directory, buildingsinfo):
         def objects_with_counts(objtype, json_):
-				element_set = {}
-				for element in json_:
-					element_set[element['name']] = str(element['amount'])
-				counts = ' '.join(element_set.values())
-				objects = [objtype.objects.get_or_create(name = w, tribe = self._to)[0] for w in element_set.keys()]
-				return counts, objects
+            element_set = {}
+            for element in json_:
+                element_set[element['name']] = str(element['amount'])
+            counts = ' '.join(element_set.values())
+            objects = [objtype.objects.get_or_create(name=w, tribe=self._to)[
+                0] for w in element_set.keys()]
+            return counts, objects
 
         enhancement_hierarchy = []
-        print "  parsing buildings"
+        print '  parsing buildings'
 
         for building in buildingsinfo['buildings']:
-				print "    " + building['name']
-				b = BuildingModel.objects.get_or_create( tribe = self._to, name = building['name'] )[0]
-				b.displayname = building['descname']
-				b.type = building['type']
+            print '    ' + building['name']
+            b = BuildingModel.objects.get_or_create(
+                tribe=self._to, name=building['name'])[0]
+            b.displayname = building['descname']
+            b.type = building['type']
 
-				# Get the building size
-				size = building['size']
-				res, = [ k for k,v in BuildingModel.SIZES if v == size ]
-				b.size = res
+            # Get the building size
+            size = building['size']
+            res, = [k for k, v in BuildingModel.SIZES if v == size]
+            b.size = res
 
-				nn = self._copy_picture(os.path.normpath(base_directory + "/" + building['icon']), building['name'], "menu.png")
-				b.image_url = nn
+            nn = self._copy_picture(os.path.normpath(
+                base_directory + '/' + building['icon']), building['name'], 'menu.png')
+            b.image_url = nn
 
-				# Buildcost if we have any
-				if 'buildcost' in building:
-					b.build_costs, b.build_wares = objects_with_counts(WareModel, building['buildcost'])
+            # Buildcost if we have any
+            if 'buildcost' in building:
+                b.build_costs, b.build_wares = objects_with_counts(
+                    WareModel, building['buildcost'])
 
-				# Try to figure out who works there
-				if 'workers' in building:
-					b.workers_count, b.workers_types  = objects_with_counts(WorkerModel, building['workers'])
+            # Try to figure out who works there
+            if 'workers' in building:
+                b.workers_count, b.workers_types = objects_with_counts(
+                    WorkerModel, building['workers'])
 
-				# Try to figure out if this building can be enhanced
-				if 'enhancement' in building:
-					enhancement_hierarchy.append((b, building['enhancement']))
+            # Try to figure out if this building can be enhanced
+            if 'enhancement' in building:
+                enhancement_hierarchy.append((b, building['enhancement']))
 
-				b.help = building['helptext']
+            b.help = building['helptext']
 
-				# Input wares
-				if 'stored_wares' in building:
-					b.store_count, b.store_wares = objects_with_counts(WareModel, building['stored_wares'])
+            # Input wares
+            if 'stored_wares' in building:
+                b.store_count, b.store_wares = objects_with_counts(
+                    WareModel, building['stored_wares'])
 
-				# Output wares
-				if 'produced_wares' in building:
-					b.output_wares = [WareModel.objects.get_or_create(name = w, tribe = self._to)[0] for w in building['produced_wares']]
+            # Output wares
+            if 'produced_wares' in building:
+                b.output_wares = [WareModel.objects.get_or_create(name=w, tribe=self._to)[
+                    0] for w in building['produced_wares']]
 
-				# Output workers
-				if 'produced_workers' in building:
-					b.output_workers = [WorkerModel.objects.get_or_create(name = w, tribe = self._to)[0] for w in building['produced_workers']]
+            # Output workers
+            if 'produced_workers' in building:
+                b.output_workers = [WorkerModel.objects.get_or_create(
+                    name=w, tribe=self._to)[0] for w in building['produced_workers']]
 
-				b.save()
+            b.save()
 
         for b, tgt in enhancement_hierarchy:
-				try:
-					b.enhancement = BuildingModel.objects.get(name = tgt, tribe = self._to)
-				except Exception, e:
-					raise
-				b.save()
+            try:
+                b.enhancement = BuildingModel.objects.get(
+                    name=tgt, tribe=self._to)
+            except Exception, e:
+                raise
+            b.save()
+
 
 class Command(BaseCommand):
     help =\
-    '''Regenerates and parses the json files in a current checkout. '''
+        '''Regenerates and parses the json files in a current checkout. '''
 
-    def handle(self, directory = os.path.normpath(WIDELANDS_SVN_DIR + "/data"), **kwargs):
+    def handle(self, directory=os.path.normpath(WIDELANDS_SVN_DIR + '/data'), **kwargs):
 
-        json_directory = os.path.normpath(MEDIA_ROOT + "/map_object_info")
-        
+        json_directory = os.path.normpath(MEDIA_ROOT + '/map_object_info')
+
         if not os.path.exists(json_directory):
             os.makedirs(json_directory)
 
-        print("JSON files will be written to: " + json_directory)
+        print('JSON files will be written to: ' + json_directory)
 
         # First, we make sure that JSON files have been generated.
         current_dir = os.path.dirname(os.path.realpath(__file__))
         is_json_valid = False
         os.chdir(WIDELANDS_SVN_DIR)
         try:
-            subprocess.check_call([os.path.normpath("wl_map_object_info"), json_directory])
+            subprocess.check_call(
+                [os.path.normpath('wl_map_object_info'), json_directory])
         except:
-            print("Error: Unable to execute 'wl_map_object_info' for generating the JSON files.")
+            print(
+                "Error: Unable to execute 'wl_map_object_info' for generating the JSON files.")
 
         # Now we validate that they are indeed JSON files (syntax check only)
-        validator_script = os.path.normpath(WIDELANDS_SVN_DIR + "/utils/validate_json.py")
+        validator_script = os.path.normpath(
+            WIDELANDS_SVN_DIR + '/utils/validate_json.py')
         if not os.path.isfile(validator_script):
-            print("Wrong path for 'utils/validate_json.py': " + validator_script + " does not exist!");
+            print("Wrong path for 'utils/validate_json.py': " +
+                  validator_script + ' does not exist!')
         try:
             subprocess.check_call(
                 [validator_script, json_directory])
             is_json_valid = True
         except:
-            print("Error: JSON files are not valid.")
+            print('Error: JSON files are not valid.')
 
         os.chdir(current_dir)
 
-        # We regenerate the encyclopedia only if the JSON files passed the syntax check
+        # We regenerate the encyclopedia only if the JSON files passed the
+        # syntax check
         if is_json_valid:
-			  source_file = open(os.path.normpath(json_directory + "/tribes.json"), "r")
-			  tribesinfo = json.load(source_file)
+            source_file = open(os.path.normpath(
+                json_directory + '/tribes.json'), 'r')
+            tribesinfo = json.load(source_file)
 
-			  for t in tribesinfo['tribes']:
-			      tribename = t['name']
-			      print "updating help for tribe ", tribename
-			      p = TribeParser(tribename)
-			      p.parse(tribename, directory, json_directory)
-			      p.graph()
+            for t in tribesinfo['tribes']:
+                tribename = t['name']
+                print 'updating help for tribe ', tribename
+                p = TribeParser(tribename)
+                p.parse(tribename, directory, json_directory)
+                p.graph()
--- wlhelp/management/commands/update_help_pdf.py
+++ wlhelp/management/commands/update_help_pdf.py
@@ -12,24 +12,27 @@ from wlhelp.models import Tribe
 from glob import glob
 import json
 
+
 class Command(BaseCommand):
     help =\
-    """Update the overview pdfs of all tribes in a current checkout"""
+        """Update the overview pdfs of all tribes in a current checkout"""
 
-    def handle(self, json_directory = os.path.normpath(MEDIA_ROOT + "/map_object_info"), **kwargs):
-        source_file = open(os.path.normpath(json_directory + "/tribes.json"), "r")
+    def handle(self, json_directory=os.path.normpath(MEDIA_ROOT + '/map_object_info'), **kwargs):
+        source_file = open(os.path.normpath(
+            json_directory + '/tribes.json'), 'r')
         tribesinfo = json.load(source_file)
 
-        print "updating pdf files for all tribes"
+        print 'updating pdf files for all tribes'
 
         for t in tribesinfo['tribes']:
             tribename = t['name']
-            print "  updating pdf file for tribe ", tribename
+            print '  updating pdf file for tribe ', tribename
             gdir = make_graph(tribename)
-            pdffile = path.join(gdir, tribename + ".pdf")
-            giffile = path.join(gdir, tribename + ".gif")
+            pdffile = path.join(gdir, tribename + '.pdf')
+            giffile = path.join(gdir, tribename + '.gif')
 
-            targetdir = path.join(MEDIA_ROOT, "wlhelp", "network_graphs", tribename)
+            targetdir = path.join(MEDIA_ROOT, 'wlhelp',
+                                  'network_graphs', tribename)
 
             try:
                 os.makedirs(targetdir)
@@ -41,10 +44,12 @@ class Command(BaseCommand):
 
             tribe = Tribe.objects.get(name=tribename)
             if tribe:
-                tribe.network_pdf_url = path.normpath("%s/%s/%s" % (MEDIA_URL, targetdir[len(MEDIA_ROOT):], tribename + ".pdf"))
-                tribe.network_gif_url = path.normpath("%s/%s/%s" % (MEDIA_URL, targetdir[len(MEDIA_ROOT):], tribename + ".gif"))
+                tribe.network_pdf_url = path.normpath(
+                    '%s/%s/%s' % (MEDIA_URL, targetdir[len(MEDIA_ROOT):], tribename + '.pdf'))
+                tribe.network_gif_url = path.normpath(
+                    '%s/%s/%s' % (MEDIA_URL, targetdir[len(MEDIA_ROOT):], tribename + '.gif'))
                 tribe.save()
             else:
-                print "Could not set tribe urls"
+                print 'Could not set tribe urls'
 
             shutil.rmtree(gdir)
