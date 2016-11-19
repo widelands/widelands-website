a35cffc93d370f7444de872050566d59ba88c534
franku <somal@arcor.de>
2016-11-19 13:26:42 +0100

    added help_text

--- news/models.py
+++ news/models.py
@@ -48,7 +48,7 @@ class Post(models.Model):
     title           = models.CharField(_('title'), max_length=200)
     slug            = models.SlugField(_('slug'), unique_for_date='publish')
     author          = models.ForeignKey(User, null=True)
-    body            = models.TextField(_('body'))
+    body            = models.TextField(_('body'), help_text="Text entered here will be rendered using Markdown")
     tease           = models.TextField(_('tease'), blank=True)
     status          = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=2)
     allow_comments  = models.BooleanField(_('allow comments'), default=True)
