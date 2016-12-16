fcbce63b9d1b95974d423ddf24561df47c48f9a7
franku <somal@arcor.de>
2016-12-16 09:26:01 +0100

    added whitespace; cleanup robots.txt

--- urls.py
+++ urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # Creating a sitemap.xml
     url(r'^sitemap\.xml/', include('sitemap_urls')),
     # Static view of robots.txt
-    url(r'^robots\.txt/',TemplateView.as_view(template_name='robots.txt', content_type="text/plain")),
+    url(r'^robots\.txt/', TemplateView.as_view(template_name='robots.txt', content_type="text/plain")),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', admin.site.urls),
