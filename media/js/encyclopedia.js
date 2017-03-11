54079da72e3e43d13b4c601d434465efcaea7306
franku <somal@arcor.de>
2017-03-11 17:03:20 +0100

    added js to filter by type of building

--- media/js/encyclopedia.js
+++ media/js/encyclopedia.js
@@ -1,30 +1,53 @@
 $(document).ready(function() {
     var elem = document.getElementById('set_size');
     elem.addEventListener('click', set_size);
-    elem = document.getElementById('set_type');
-    elem.addEventListener('click', set_type);
+    checkb_names = ['sizes', 'types']; 
     // Initialize after reload, e.g. pressing F5
-    init_checkboxes('sizes');
-    //init_checkboxes('types');
+    init_checkboxes();
 });
 
 function set_size(){
-    var option_boxes = document.getElementsByName("sizes");
-    for (var i = 0; i < option_boxes.length; i++) {
-        elements = document.getElementsByName(option_boxes[i].value);
-        for (var x = 0 ; x < elements.length; x++){
-            if (option_boxes[i].checked){
-                elements[x].style.display = '';
-            }else{
-                elements[x].style.display = 'none';    
+    for (var y=0; y<checkb_names.length; y++){
+        var option_boxes = document.getElementsByName(checkb_names[y]);
+        for (var i = 0; i < option_boxes.length; i++) {
+            elements = document.getElementsByName(option_boxes[i].value);
+            for (var x = 0 ; x < elements.length; x++){
+                if (option_boxes[i].checked){
+                    elements[x].style.display = '';
+                }else{
+                    elements[x].style.display = 'none';    
+                }
             }
+        }
+    }
+    hide_tables();
+}
+
+function hide_tables(){
+    var tables = document.getElementsByTagName('table');
+    for (var i=0; i<tables.length; i++){
+        var table = tables[i];
+        var hidden_rows = 0;
+        for (var x=0, row; row = table.rows[x]; x++){
+            if (row.style.display == 'none'){
+                hidden_rows++;
+            }
+        }
+        if (table.rows.length == hidden_rows+1){
+            table.style.display = 'none';
+            table.previousElementSibling.style.display = 'none';
+        }else{
+            table.style.display = '';
+            table.previousElementSibling.style.display = '';            
         }
     }
 }
 
-function init_checkboxes(n){
-    var option_boxes = document.getElementsByName(n);
-    for (i=0; i < option_boxes.length; i++){
-        option_boxes[i].checked = true;
+function init_checkboxes(){
+    for (var x=0; x<checkb_names.length; x++){
+        var option_boxes = document.getElementsByName(checkb_names[x]);
+        for (i=0; i < option_boxes.length; i++){
+            option_boxes[i].checked = true;
+        }
     }
 }
