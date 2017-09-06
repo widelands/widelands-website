e5d0eb77b8547e9b8710dc267e99ac08e1bc9be6
franku <somal@arcor.de>
2017-09-06 18:57:52 +0200

    added datepicker; cleanups; css tweaks

--- wlsearch/forms.py
+++ wlsearch/forms.py
@@ -1,43 +1,57 @@
-#!/usr/bin/env python -tt
-# encoding: utf-8
-#
-# Created by Holger Rapp on 2009-03-15.
-#
-# Last Modified: $Date$
-#
-
+from haystack.forms import SearchForm
+from haystack.utils.app_loading import haystack_get_model
 from django import forms
+from datetime import date, timedelta
 
 
-class SearchForm(forms.Form):
-    search = forms.CharField(max_length=200)
-    incl_wiki = forms.BooleanField(required=False, initial=True, label='Wiki')
+class WlSearchForm(SearchForm):
+
+    start_date = forms.DateField(
+        required=False, initial=date.today() - timedelta(365*2),
+        widget=forms.TextInput(attrs=
+                               {'size': '10',
+                                #'placeholder': 'YYYY-MM-DD',
+                                'class': 'datepicker',
+                                }))
     incl_forum = forms.BooleanField(
         required=False, initial=True, label='Forum')
-    incl_news = forms.BooleanField(required=False, initial=True, label='News')
-    incl_maps = forms.BooleanField(required=False, initial=True, label='Maps')
+    incl_maps = forms.BooleanField(
+        required=False, initial=True, label='Maps')
+    incl_wiki = forms.BooleanField(
+        required=False, initial=True, label='Wiki')
     incl_help = forms.BooleanField(
-        required=False, initial=True, label='Online Help')
+        required=False, initial=True, label='Encyclopedia')
+    incl_news = forms.BooleanField(
+        required=False, initial=True, label='News')
 
-from haystack.forms import ModelSearchForm
-import datetime
-
-class WlSearchForm(ModelSearchForm):
-    
-    start_date = forms.DateField(widget=forms.TextInput(attrs={'size': '10', 'placeholder':'YYYY-MM-DD'}), required=False)
-    
-    def search(self):      
-        #First, store the SearchQuerySet received from other processing.
+    def search(self):
+        # First, store the SearchQuerySet received from other processing.
         sqs = super(WlSearchForm, self).search()
-    
-        if not self.is_valid():
-            return self.no_query_found()
 
-        print('is valid', self.cleaned_data)
+        # Collect the models to search for
+        search_models = []
+        if self.cleaned_data['incl_forum']:
+            search_models.append(haystack_get_model('pybb', 'topic'))
+            search_models.append(haystack_get_model('pybb', 'post'))
+        if self.cleaned_data['incl_maps']:
+            search_models.append(haystack_get_model('wlmaps', 'map'))
+        # if self.cleaned_data['incl_wiki']:
+        #     search_models.append(haystack_get_model('wiki', 'article'))
+        if self.cleaned_data['incl_help']:
+            search_models.append(haystack_get_model('wlhelp', 'worker'))
+            # search_models.append(haystack_get_model('wlhelp', 'tribe'))
+            # search_models.append(haystack_get_model('wlhelp', 'ware'))
+            # search_models.append(haystack_get_model('wlhelp', 'building'))
+        # if self.cleaned_data['incl_news']:
+        #     search_models.append(haystack_get_model('news', 'post'))
+        # Add the chosen models to the query
+        if search_models:
+            sqs = sqs.models(*search_models)
 
         # Check to see if a start_date was chosen.
         if self.cleaned_data['start_date']:
-            print('franku start_date: ', self.cleaned_data['start_date'])
-            sqs = sqs.filter(date__gte=self.cleaned_data['start_date']).order_by('-date')
-    
+            sqs = sqs.filter(date__gte=self.cleaned_data[
+                             'start_date']).order_by('-date')
+
+        # Run the query
         return sqs
--- wlsearch/views.py
+++ wlsearch/views.py
@@ -1,113 +1,27 @@
-# Create your views here.
-
-
 from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response, render, redirect
-from django.template import RequestContext
-
-from forms import SearchForm
-
-from wiki.models import Article
-from pybb.models import Post, Topic
-from news.models import Post as NewsPost
-from wlhelp.models import Building, Ware
-from wlmaps.models import Map
-
-class DummyEmptyQueryset(object):
-    """A simple dummy class when a search should not be run.
-
-    The template expects a queryset and checks for the count member.
-
-    """
-
-    def count(self):
-        return 0
-
-
-def search(request):
-    if request.method == 'POST':
-        form = SearchForm(request.POST)
-
-        if form.is_valid():
-            query = form.cleaned_data['search']
-            do_wiki = form.cleaned_data['incl_wiki']
-            do_forum = form.cleaned_data['incl_forum']
-            do_news = form.cleaned_data['incl_news']
-            do_help = form.cleaned_data['incl_help']
-            do_maps = form.cleaned_data['incl_maps']
-
-            # Help
-            wlhelp_wares = Ware.search.query(
-                query) if do_help else DummyEmptyQueryset()
-            wlhelp_buildings = Building.search.query(
-                query) if do_help else DummyEmptyQueryset()
-
-            # Maps
-            map_results = Map.search.query(
-                query) if do_maps else DummyEmptyQueryset()
-
-            # Wiki
-            wiki_results = Article.search.query(
-                query) if do_wiki else DummyEmptyQueryset()
-
-            # Forum
-            forum_results_topic = Topic.search.query(
-                query) if do_forum else DummyEmptyQueryset()
-            forum_results_post = Post.search.query(
-                query) if do_forum else DummyEmptyQueryset()
-
-            # News
-            news_results = NewsPost.search.query(
-                query) if do_news else DummyEmptyQueryset()
-
-            template_params = {
-                'wiki_results': wiki_results,
-
-                'wlhelp_hits': wlhelp_wares.count() + wlhelp_buildings.count(),
-                'wlhelp_results_wares': wlhelp_wares,
-                'wlhelp_results_buildings': wlhelp_buildings,
-
-                'forum_hits': forum_results_post.count() + forum_results_topic.count(),
-                'forum_results_topic': forum_results_topic,
-                'forum_results_post': forum_results_post,
-
-                'map_results': map_results,
-
-                'news_results': news_results,
-
-                'search_form': form,
-                'post': True,
-            }
-
-            return render_to_response('wlsearch/search.html',
-                                      template_params,
-                                      context_instance=RequestContext(request))
-    else:
-        form = SearchForm()
-
-    template_params = {
-        'search_form': form,
-        'post': False,
-    }
-    return render_to_response('wlsearch/search.html',
-                              template_params,
-                              context_instance=RequestContext(request))
-
-
+from django.shortcuts import render
 from haystack.generic_views import SearchView
-from haystack.forms import ModelSearchForm as HaystackForm
 from forms import WlSearchForm
-from haystack.query import EmptySearchQuerySet, SearchQuerySet
-from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from datetime import date, timedelta
+
 
 class HaystackSearchView(SearchView):
     """My custom search view."""
     template_name = 'search/search_test.html'
     form_class = WlSearchForm
     paginate_by = None
+
+
+    def get(self, request, *args, **kwargs):
+        """Used to return form errors"""
+        form = self.form_class(request.GET)
+        if form.is_valid():
+            form.search()
+            return super(HaystackSearchView, self).get(form)
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
         """ This is executed when searching through the box in the navigation
@@ -117,46 +31,43 @@ class HaystackSearchView(SearchView):
         """
         form = self.form_class(request.POST)
         if form.is_valid() and form.cleaned_data['q'] != '':
-            print('franku cleaned_data: ', form.cleaned_data)
+            print('franku cleaned_data: ',form.cleaned_data)
+            # Allways set the search string
             search_url = 'q=%s' % (form.cleaned_data['q'])
-            # Search only the last two years from today
-            search_url += '&start_date=%s' % (date.today() - timedelta(365*2))
-
-            for model in form.cleaned_data['models']:
-                search_url += '&models=%s' % (model)
+            # add initial values of the form fields
+            for field, v in form.fields.iteritems():
+                if field =='q':
+                    continue
+                search_url += '&%s=%s' % (field, v.initial)
             return HttpResponseRedirect('%s?%s' % (reverse('search'), search_url))
+        form = self.form_class
         return render(request, self.template_name, {'form': form})
 
     def get_context_data(self, *args, **kwargs):
         """ Regrouping the search results """
     
         context = super(HaystackSearchView, self).get_context_data(*args, **kwargs)
-        #print('franku context kwargs: ', args, kwargs)
-        # for k,v in context.iteritems():
-        #     print('franku context data: ', k, " : ", v)
-        # print('franku object_list: ', context['object_list'])
-        if context['object_list'] != EmptySearchQuerySet:
-            maps = []
-            topics = []
-            posts = []
-            workers = []
-            for item in context['object_list']:
-                #print('franku item: ', item)
-                if item.content_type() == 'wlmaps.map':
-                    maps.append(item)
-                if item.content_type() == "pybb.post":
-                    posts.append(item)
-                if item.content_type() == "pybb.topic":
-                    topics.append(item)
-                if item.content_type() == "wlhelp.worker":
-                    workers.append(item)
-            
-            sorted_objects = [
-                {'maps': maps},
-                {'topics': topics},
-                {'posts': posts},
-                {'workers': workers},
-            ]
-            context['object_list'] = sorted_objects
+        maps = []
+        topics = []
+        posts = []
+        workers = []
+        for item in context['object_list']:
+            #print('franku item: ', item)
+            if item.content_type() == 'wlmaps.map':
+                maps.append(item)
+            if item.content_type() == "pybb.post":
+                posts.append(item)
+            if item.content_type() == "pybb.topic":
+                topics.append(item)
+            if item.content_type() == "wlhelp.worker":
+                workers.append(item)
+        
+        sorted_objects = [
+            {'maps': maps},
+            {'topics': topics},
+            {'posts': posts},
+            {'workers': workers},
+        ]
+        context['object_list'] = sorted_objects
         
         return context
