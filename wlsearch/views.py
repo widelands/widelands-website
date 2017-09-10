5f4f1935ccef2297136b3f0f830fdf59ed9cdd42
franku <somal@arcor.de>
2017-09-10 15:38:24 +0200

    use list comprehensions; adjusted result page

--- wlsearch/views.py
+++ wlsearch/views.py
@@ -15,7 +15,7 @@ class HaystackSearchView(SearchView):
 
 
     def get(self, request, *args, **kwargs):
-        """Used to return form errors"""
+        """Used to return form errors."""
         form = self.form_class(request.GET)
         if form.is_valid():
             form.search()
@@ -24,10 +24,10 @@ class HaystackSearchView(SearchView):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        """ This is executed when searching through the box in the navigation
-        
+        """ This is executed when searching through the box in the navigation.
+
         We build the query string and redirect it to this view again.
-    
+
         """
         form = self.form_class(request.POST)
         if form.is_valid() and form.cleaned_data['q'] != '':
@@ -35,7 +35,7 @@ class HaystackSearchView(SearchView):
             search_url = 'q=%s' % (form.cleaned_data['q'])
             # add initial values of the form fields
             for field, v in form.fields.iteritems():
-                if field =='q':
+                if field == 'q':
                     continue
                 search_url += '&%s=%s' % (field, v.initial)
             return HttpResponseRedirect('%s?%s' % (reverse('search'), search_url))
@@ -43,62 +43,35 @@ class HaystackSearchView(SearchView):
         return render(request, self.template_name, {'form': form})
 
     def get_context_data(self, *args, **kwargs):
-        """ Regrouping the search results """
-    
-        context = super(HaystackSearchView, self).get_context_data(*args, **kwargs)
-        # new_context = {}
-        # for i in context['object_list']:
-        #     print('franku context: ', i.app_label, i.model_name, i)
-        #     if i.app_label in new_context:
-        #         pass
-        #     else:
-        #         model = {i.model_name}
-        #         new_context[i.app_label] = model
-        #         print('frank new context:' , new_context)
-        #         new_context[i.app_label].model_name.append(i)
-        # print('frank new context:' , new_context)
-        maps = []
-        topics = []
-        posts = []
-        workers = []
-        wares = []
-        buildings = []
-        tribes = []
-        for item in context['object_list']:
-            #print('franku item: ', item)
-            if item.content_type() == 'wlmaps.map':
-                maps.append(item)
-            if item.content_type() == "pybb.post":
-                posts.append(item)
-            if item.content_type() == "pybb.topic":
-                topics.append(item)
-            if item.content_type() == "wlhelp.worker":
-                workers.append(item)
-            if item.content_type() == "wlhelp.ware":
-                wares.append(item)
-            if item.content_type() == "wlhelp.building":
-                buildings.append(item)
-            if item.content_type() == "wlhelp.tribe":
-                tribes.append(item)
-                
-        sorted_objects = [
-            {'maps': maps},
-            {'topics': topics},
-            {'posts': posts},
-            {'wares': wares},
-            {'buildings': buildings},
-            {'tribes': tribes},
-            {'workers': workers},
-            # {'wlmaps': [{'maps': maps}]},
-            # {'forum': [{'topics': topics},
-            #             {'posts': posts},]
-            # },
-            # {'encyclopedia': [{'wares': wares},
-            #     {'buildings': buildings},
-            #     {'tribes': tribes},
-            #     {'workers': workers}]
-            #  },
-        ]
+        """Regrouping the search results."""
+
+        context = super(HaystackSearchView, self).get_context_data(
+            *args, **kwargs)
+        
+        maps = [m for m in context['object_list'] if m.content_type() == "wlmaps.map"]
+        topics = [t for t in context['object_list'] if t.content_type() == "pybb.topic"]
+        posts = [p for p in context['object_list'] if p.content_type() == "pybb.post"]
+        workers = [w for w in context['object_list'] if w.content_type() == "wlhelp.worker"]
+        buildings = [b for b in context['object_list'] if b.content_type() == "wlhelp.building"]
+        wares = [w for w in context['object_list'] if w.content_type() == "wlhelp.ware"]
+        tribes = [t for t in context['object_list'] if t.content_type() == "wlhelp.tribe"]
+
+        sorted_objects = []
+        # Put all found things into the context, omit if nothing was found
+        # so the context is as small as possible
+        if len(maps) > 0 :
+            sorted_objects.append({'wlmaps': {'maps': maps}})
+        if len(topics) > 0 or len(posts) > 0:
+            sorted_objects.append({'forum': {'topics': topics,
+                       'posts': posts,
+                       }},)
+        if len(workers) or len(buildings) or len(wares) or len(tribes):
+            sorted_objects.append({'encyclopedia': {'wares': wares,
+                              'buildings': buildings,
+                              'tribes': tribes,
+                              'workers': workers,
+                              }})
+
         context['object_list'] = sorted_objects
         
         return context
