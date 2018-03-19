7e98fbdcc5223cde0266b2c771662da280d75844
franku <somal@arcor.de>
2018-03-19 08:34:49 +0100

    modified news admin page

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from news.models import *
-
+from datetime import datetime
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
@@ -11,13 +11,23 @@ admin.site.register(Category, CategoryAdmin)
 class PostAdmin(admin.ModelAdmin):
 
     def get_changeform_initial_data(self, request):
-        # Set initial Data for the ForeignKey field to prevent
+        # Set initial value for the ForeignKey field to prevent
         # digging through the users list
-        return {'author': request.user}
+        return {'author': request.user,
+                'publish': datetime.now(),}
 
     list_display = ('title', 'publish', 'status')
     list_filter = ('publish', 'categories', 'status')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        (None, {
+            'fields':(('title', 'slug'), 'body', ('publish', 'categories',), )
+        }),
+        ('More options', {
+            'classes': ('collapse',),
+            'fields': ('author', ('status', 'allow_comments',), 'tease', 'tags')
+        }),
+    )
 
 admin.site.register(Post, PostAdmin)
