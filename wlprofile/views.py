9c0172f6be95eca132d73c7b2424564f6c67530f
franku <somal@arcor.de>
2018-09-07 18:33:58 +0200

    Added view function to delete the users data

--- wlprofile/views.py
+++ wlprofile/views.py
@@ -4,7 +4,7 @@
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 
@@ -13,14 +13,62 @@ import settings
 
 
 @login_required
-def delete_me(request, user=None):
-    """Delete the user"""
-    
+def delete_me(request):
+    """Show a page to inform the user what deleting means."""
+
     context = {
         'user': request.user
     }
     return render(request, 'wlprofile/delete_me.html',
                   context)
+
+
+def do_delete(request):
+    """Delete user specific data.
+
+    We can't delete a user but do some cleanup.  
+    """
+
+    from django.contrib.auth.models import User
+    from django.contrib.auth import logout
+
+    user = get_object_or_404(User, username=request.user)
+
+    # Log the user out. This must be done as early as possible but after
+    # we get the User object.
+    logout(request)
+
+    # Deleting the profile removes private settings, e.g. the avatar
+    # TODO(franku): Delete also the image file (not the anonymus.png)
+    profile = user.wlprofile
+    profile.delete()
+
+    # Deactivate all subscriptions
+    from notification.models import NoticeSetting
+    settings = NoticeSetting.objects.filter(user=user)
+    for setting in settings:
+        setting.send = False
+        setting.save()
+
+    # Remove written PMs from outbox
+    from django_messages.models import Message
+    messages = Message.objects.outbox_for(user)
+    for message in messages:
+        message.delete()
+    # Remove PMs which are in the trash of sender and recipients
+    messages = Message.objects.trash_for(user)
+    for message in messages:
+        message.delete()
+    # TODO(franku): Prevend sending PMs to a deactivated user
+
+    # Do some settings in Django
+    user.is_active = False
+    user.is_staff = False
+    user.is_superuser = False
+    user.email = 'deleted@wl.org'
+    user.save()
+    #return redirect('delete_me')
+    return redirect('mainpage')
 
 
 @login_required
