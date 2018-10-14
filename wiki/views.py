1021effa39ca6bef825398d2995c349aa66ccaed
franku <somal@arcor.de>
2018-10-14 21:42:00 +0200

    small codng fix

--- wiki/views.py
+++ wiki/views.py
@@ -672,6 +672,7 @@ def backlinks(request, title):
     context = {'found_links': found_links,
                'found_old_links': found_old_links,
                'name': title,
-               'article': this_article,}
+               'article': this_article,
+               }
     return render(request, 'wiki/backlinks.html',
                               context,)
