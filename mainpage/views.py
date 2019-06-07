654ff7a8d21518326bed898d5de8ce8a2de87e7a
kaputtnik <>
2019-06-07 22:25:05 +0200

    added output of DISPLAY environment variable to /locale/

--- mainpage/views.py
+++ mainpage/views.py
@@ -160,5 +160,7 @@ def view_locale(request):
     loc_info = 'getlocale: ' + str(locale.getlocale()) + \
         '<br/>getdefaultlocale(): ' + str(locale.getdefaultlocale()) + \
         '<br/>fs_encoding: ' + str(sys.getfilesystemencoding()) + \
-        '<br/>sys default encoding: ' + str(sys.getdefaultencoding())
+        '<br/>sys default encoding: ' + str(sys.getdefaultencoding()) + \
+        '<br><br> Environment variables:' + \
+        '<br>DISPLAY: ' + os.environ.get('DISPLAY', 'Not set')
     return HttpResponse(loc_info)
