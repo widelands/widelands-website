3854104f334ff1c401d081d4928e4e96b0eabc4c
Holger Rapp <SirVer@gmx.de>
2019-06-21 13:28:01 +0200

    Updated ops script for use on new server.

--- _ops/apt_update.sh
+++ _ops/apt_update.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
 # Updates all packages on the server, but stops the widelands website before
 # doing so, in case mysql gets updated - it always results in really ugly
-# errors for users otherwise. Ideally, this script would switch the website to
-# a "In Maintenance" banner.
+# errors for users otherwise.
 #
-# This script requires root access.
+# This script requires sudo.
 
 set -ex
 
-if [ -z "$STY" ] && [ -z "$TMUX" ]; then 
+if [ -z "${TMUX}" ]; then 
    echo "Run inside screen or tmux in case SSH gets updated."
    exit 1
 fi
 
-apt-get update
-stop wlwebsite || true
+sudo apt update
 
-# TODO(sirver): Upgrading widelands-data takes a long time (~30 minutes or
-# longer). Use apt-mark hold to not update widelands and widelands-data to
-# bring the website up quicker again. Then only upgrade those packages later,
-# after the website is up again.
-# See http://askubuntu.com/questions/99774/exclude-packages-from-apt-get-upgrade
-apt-get dist-upgrade
+sudo systemctl start wl-bauarbeiten
 
-start wlwebsite
+sudo apt dist-upgrade
 
-apt-get autoremove -y
+sudo systemctl start wl-website
+
+sudo apt autoremove -y
