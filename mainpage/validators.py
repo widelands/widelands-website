4169239cd302fce135642e6aca0136c8130ab305
kaputtnik <>
2019-08-25 17:04:10 +0200

    use an absolute path for clamdscan. This is needed on the server

--- mainpage/validators.py
+++ mainpage/validators.py
@@ -9,16 +9,22 @@ def virus_scan(uploaded_file):
     '''Scan uploaded_file for viruses with clamav.'''
 
     if settings.VIRUS_CHECK:
-        tmp_file_path = uploaded_file.temporary_file_path()
-        process_compl = subprocess.run(['clamdscan',
-                                        '--multiscan',
-                                        '--fdpass',
-                                        tmp_file_path])
-        if process_compl.returncode == 1:
-            raise ValidationError(
-                'This file seems to contain malicious code.'
+        try:
+            tmp_file_path = uploaded_file.temporary_file_path()
+            process_compl = subprocess.run(['/usr/bin/clamdscan',
+                                            '--multiscan',
+                                            '--fdpass',
+                                            tmp_file_path])
+            if process_compl.returncode == 1:
+                raise ValidationError(
+                    'This file seems to contain malicious code.'
+                    )
+            if process_compl.returncode == 2:
+                raise ValidationError(
+                    'Some error occured during virus scannning...'
                 )
-        if process_compl.returncode == 2:
+        except FileNotFoundError:
             raise ValidationError(
-                'Some error occured during virus scannning...'
+                'Please check the installation of clamav and make \
+                sure clamdscan is working.'
             )
