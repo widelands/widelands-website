f41af94e960375cc5e5c77f60b7be3cbf8657aac
Geoffroy Schmitlin <geoffroy.schmitlin@phiropsi.com>
2019-09-09 00:10:08 +0200

    Added urls (last commit miss) and admin ui

--- wlrating/admin.py
+++ wlrating/admin.py
@@ -2,11 +2,30 @@ from django.contrib import admin
 from .models import Game, Participant, Player_Rating, Temporary_user, Season
 
 
-admin.site.register(Participant)
-admin.site.register(Player_Rating)
-admin.site.register(Temporary_user)
 admin.site.register(Season)
 
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('get_username', 'get_user_id', 'game', 'team', 'submitter', 'tribe')
+
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = 'username'
+
+    def get_user_id(self, obj):
+        return obj.user.id
+    get_user_id.short_description = 'user id'
+
+admin.site.register(Participant, ParticipantAdmin)
+
+
+class PlayerRatingAdmin(admin.ModelAdmin):
+    list_display = ('get_username', 'rating_type', 'decimal1', 'decimal2', 'decimal3', 'season')
+
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = 'User'
+
+admin.site.register(Player_Rating, PlayerRatingAdmin)
 
 class GameAdmin(admin.ModelAdmin):
     list_display = ('start_date', 'game_type', 'game_map', 'win_team', 'game_status', 'game_breaks', 'counted_in_score', 'get_participants')
@@ -18,3 +37,7 @@ class GameAdmin(admin.ModelAdmin):
 
 admin.site.register(Game, GameAdmin)
 
+class TemporaryUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'id')
+
+admin.site.register(Temporary_user, TemporaryUserAdmin)
--- wlrating/urls.py
+++ wlrating/urls.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python -tt
 # encoding: utf-8
 from django.conf.urls import *
-from .view import rating_main, arbiter, score
+from .view import rating_main, arbiter, score, remove_btn, calculate_scores, add_test_data
 
 urlpatterns = [
     url(r'^main/$', rating_main, name='rating_main'),
     url(r'^arbiter/$', arbiter, name='arbiter'),
+    url(r'^remove_btn/(?P<game_id>[0-9]+)$', remove_btn, name='remove_btn'),
+    url(r'^calculate_scores/$', calculate_scores, name='calculate_scores'),
+    url(r'^add_test_data/$', add_test_data, name='add_test_data'),
     url(r'^score/$', score, name='score'),
 ]
