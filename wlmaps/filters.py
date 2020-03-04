9da8d8c9100799ac67790a0139c790d129dc41ee
Arne Schauf <git@asw.io>
2020-03-05 00:44:19 +0100

    add order by rating and filter by required version to map list

--- wlmaps/filters.py
+++ wlmaps/filters.py
@@ -7,12 +7,14 @@ class MapFilter(django_filters.FilterSet):
     nr_players = django_filters.RangeFilter()
     w = django_filters.RangeFilter()
     h = django_filters.RangeFilter()
+    wl_version_after = django_filters.NumberFilter(label='Max required version', lookup_expr='lt')
     o = django_filters.OrderingFilter(
-        fields=('pub_date', 'name', 'author', 'w', 'h'),
+        fields=('pub_date', 'name', 'author', 'w', 'h', 'ratings__average'),
         field_labels={
             'pub_date': 'Upload date',
             'w': 'Width',
             'h': 'Height',
+            'ratings__average': 'Average rating',
         }
     )
 
