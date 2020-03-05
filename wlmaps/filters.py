242dcd77d2ef2797ea7fe28c7128204194660182
Arne Schauf <git@asw.io>
2020-03-05 12:50:47 +0100

    make range fields smaller and use select for version

--- wlmaps/filters.py
+++ wlmaps/filters.py
@@ -3,11 +3,20 @@ import django_filters
 from . import models
 
 
+VERSION_CHOICES = (
+    ('19', 'build 19 or newer'),
+    ('20', 'build 20 or newer'),
+    ('21', 'dev'),
+)
+
+
 class MapFilter(django_filters.FilterSet):
     nr_players = django_filters.RangeFilter()
     w = django_filters.RangeFilter()
     h = django_filters.RangeFilter()
-    wl_version_after = django_filters.NumberFilter(label='Max required version', lookup_expr='lt')
+    wl_version_after = django_filters.TypedChoiceFilter(
+        label='Required version', lookup_expr='lt', choices=VERSION_CHOICES, coerce=lambda x: int(x)
+    )
     o = django_filters.OrderingFilter(
         fields=('pub_date', 'name', 'author', 'w', 'h', 'ratings__average', 'nr_downloads'),
         field_labels={
