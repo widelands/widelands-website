a6874a1e9279d749efd004247ffe6bbc00fd31e5
Arne Schauf <git@asw.io>
2020-03-05 11:23:12 +0100

    relayout maps filter form, add downloads to ordering

--- wlmaps/filters.py
+++ wlmaps/filters.py
@@ -9,12 +9,13 @@ class MapFilter(django_filters.FilterSet):
     h = django_filters.RangeFilter()
     wl_version_after = django_filters.NumberFilter(label='Max required version', lookup_expr='lt')
     o = django_filters.OrderingFilter(
-        fields=('pub_date', 'name', 'author', 'w', 'h', 'ratings__average'),
+        fields=('pub_date', 'name', 'author', 'w', 'h', 'ratings__average', 'nr_downloads'),
         field_labels={
             'pub_date': 'Upload date',
             'w': 'Width',
             'h': 'Height',
             'ratings__average': 'Average rating',
+            'nr_downloads': 'Downloads',
         }
     )
 
