769dfc205efc86c94341b8acb770fa2f90e051ff
kaputtnik <somal@arcor.de>
2020-09-29 19:48:44 +0200

    refactoring

--- pybb/templatetags/pybb_extras.py
+++ pybb/templatetags/pybb_extras.py
@@ -119,25 +119,19 @@ def pybb_moderated_by(instance, user):
 def pybb_editable_by(post, user):
     """Check if a user is allowed to edit this post."""
 
+    if not user.is_authenticated():
+        # Speed up if the user isn't online
+        return False
     if user.is_superuser:
         return True
     if user in post.topic.forum.moderator_group.user_set.all():
+        # Forum moderators are always allowed
         return True
     if post.user == user:
         # Restrict the time a user can edit his own post
         edit_time = timedelta(hours=pybb_settings.EDIT_HOURS)
-        if datetime.now() - edit_time > post.created:
-            return False
-        else:
-            return True
+        return datetime.now() <= post.created + edit_time
     return False
-
-
-@register.filter
-def pybb_posted_by(post, user):
-    """Check if the post is writed by the user."""
-
-    return post.user == user
 
 
 @register.filter
