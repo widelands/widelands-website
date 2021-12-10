89ed0b622d6dba089b6158c8fe74f9ee200dc6a6
kaputtnik <somal@arcor.de>
2021-12-10 18:58:18 +0100

    comments for settings

--- wladdons_settings/settings.py
+++ wladdons_settings/settings.py
@@ -1,3 +1,7 @@
+# Default settings for retrieving Email settings for the add-ons server
+# !!! Do not edit the main keys once this got merged !!!
+# The values for 'display' and 'description can be edited after merging, they 
+# will be updated if users enters the respective profile tab.
 
 ADDONNOTICETYPES = {
     'addon_deleted': {'display': 'Your Add-On was deleted',
