8bd2f4345bcd297925e5d8e64a2633926c7a1ad8
kaputtnik <6730556+frankystone@users.noreply.github.com>
2022-01-10 16:37:25 +0100

    Lua class graphs (website) (#357)
    * start for class graphs
    
    * removed extension inheritance diagram
    
    * graphviz css
    
    * adjusted pygemnts.css
    
    * add option -graphs; some css tweaks
    
    * using just clear: both;
    
    * codeblocks are not overlapping class graphs
    
    * define max-width: 80% for graphs
    
    Co-authored-by: kaputtnik <somal@arcor.de>

--- documentation/conf.py
+++ documentation/conf.py
@@ -26,7 +26,7 @@
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax']
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.graphviz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -169,3 +169,4 @@ html_use_index = True
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Widelandsdoc'
+graphviz_output_format = 'svg'
