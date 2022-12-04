3b3d4866ebfc7da873b096b78f9325ab60afda36
kaputtnik <6730556+frankystone@users.noreply.github.com>
2022-12-04 11:09:10 +0100

    feeds redirect (#396)
    * added redirect for news feeds
    
    * added comment, ran black
    
    Co-authored-by: kaputtnik <somal@arcor.de>

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -68,6 +68,11 @@ urlpatterns = [
     url(r"^scheduling/", include("wlscheduling.urls")),
     url(r"^privacy/", include("privacy_policy.urls")),
     url(r"^addons/", include("wladdons_settings.urls")),
+    # See: https://github.com/widelands/widelands-website/issues/376
+    url(
+        r"^feeds/news/",
+        RedirectView.as_view(url="/news/feed", permanent=True),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 try:
