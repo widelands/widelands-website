4f19179430a440e66b26f3a6a2a4a457c167f9bc
janus <widelands-github@jhor.de>
2022-12-12 23:04:43 +0100

    add missing ()

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -49,9 +49,9 @@ class LoginTimezoneForm(AuthenticationForm):
         profile = get_object_or_404(
             User, username=cleaned_data.get("username")
         ).wlprofile
-        if set_timezone
+        if (set_timezone
                 and br_time_zone is not None
-                and profile.time_zone != br_time_zone:
+                and profile.time_zone != br_time_zone):
             found = False
             for value, display in TZ_CHOICES:
                 if value == br_time_zone:
