6b6d12ac38eefd034a884160473fef8cde2bcd69
janus <widelands-github@jhor.de>
2022-12-26 19:06:55 +0100

    add subject check

--- django_messages_wl/forms.py
+++ django_messages_wl/forms.py
@@ -6,3 +6,4 @@ class ExtendedComposeForm(ComposeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["body"].validators.append(check_utf8mb3)
+        self.fields["subject"].validators.append(check_utf8mb3)
