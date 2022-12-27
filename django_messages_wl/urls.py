f04a52d80492df00938eaf3139fdb2d7f06ae8c4
kaputtnik <6730556+frankystone@users.noreply.github.com>
2022-12-27 19:01:54 +0100

    Catch 4byte chars without thc (#409)
    * through error on posting 4 byte characters
    
    * autoformatted by black
    
    * validate threadedcomments, wlprofile
    
    * autoformatted by black
    
    * try to fix threadedcomments, not ready yet
    
    * fixed threadedcomment, not sure if this works correct on the server
    
    * next try
    
    * add validator to django-messages
    
    * removed chnges to thc
    
    * autoformatted by black
    
    * removed print
    
    * forgotten to restore views and template
    
    * url fix fod django-messages
    
    * fixed also urls for reply
    
    * autoformatted by black
    
    * add subject check
    
    * colored error for pybb preview
    
    * style what ever :)
    
    Co-authored-by: kaputtnik <somal@arcor.de>
    Co-authored-by: frankystone <frankystone@users.noreply.github.com>
    Co-authored-by: janus <widelands-github@jhor.de>

--- django_messages_wl/urls.py
+++ django_messages_wl/urls.py
@@ -1,8 +1,29 @@
 from django.conf.urls import *
-from django_messages.urls import urlpatterns
 from . import views
+from django_messages.views import compose, reply
+from django_messages_wl.forms import ExtendedComposeForm
 
-
-urlpatterns += [
+urlpatterns = [
     url(r"^django_messages_wl/get_usernames/", views.get_usernames),
+    # Overridden urls to add a custom validator
+    url(
+        r"^compose/$",
+        compose,
+        {"form_class": ExtendedComposeForm},
+        name="messages_compose",
+    ),
+    url(
+        r"^compose/(?P<recipient>[\w.@+-]+)/$",
+        compose,
+        {"form_class": ExtendedComposeForm},
+        name="messages_compose_to",
+    ),
+    url(
+        r"^reply/(?P<message_id>[\d]+)/$",
+        reply,
+        {"form_class": ExtendedComposeForm},
+        name="messages_reply",
+    ),
+    # Needs to be after the custom views
+    url(r"^", include("django_messages.urls")),
 ]
