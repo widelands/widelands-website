b757b13f82145c2310e08a8254c9c56724af89d9
frankystone <frankystone@users.noreply.github.com>
2023-02-09 17:31:56 +0000

    autoformatted by black

--- wlprofile/views.py
+++ wlprofile/views.py
@@ -132,7 +132,7 @@ def view(request, user=None):
 
     template_params = {
         "profile": profile,
-        "addondb": settings.DATABASES.get("addonserver")
+        "addondb": settings.DATABASES.get("addonserver"),
     }
 
     return render(request, "wlprofile/view_profile.html", template_params)
