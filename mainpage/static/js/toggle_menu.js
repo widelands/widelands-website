0ea9ee5df77052a31af404a22ae8380b3c09f8fa
frankystone <>
2025-01-20 12:49:36 +0100

    first working solution for responsive login

--- mainpage/static/js/toggle_menu.js
+++ mainpage/static/js/toggle_menu.js
@@ -4,3 +4,21 @@ $( function() {
           $( ".responsiveHidden" ).toggle( "slow" );
     });
 });
+
+$( function() {
+    $( "#loginButton" ).on( "click", function() {
+          $( "#responsiveLogin" ).toggle( "slow" );
+    });
+});
+/*
+function toggleHidden(cl) {
+  var x = document.getElementsByClassName(cl);
+  */
+/*if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }*//*
+
+}
+*/
