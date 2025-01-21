63dd29defdbfeaee90c24c7048d3958ffb5ca254
frankystone <>
2025-01-21 17:52:38 +0100

    first working button-solution for devices with no hover effect

--- mainpage/static/js/toggle_menu.js
+++ mainpage/static/js/toggle_menu.js
@@ -1,4 +1,4 @@
-
+/* Functions to hide some elements on small viewports */
 $( function() {
     $( "#toggleMenu" ).on( "click", function() {
           $( ".respMenuHidden" ).toggle( "slow" );
@@ -16,6 +16,57 @@ $( function() {
           $( ".columnModule" ).toggle( "slow" );
     });
 });
+
+/*
+    Buttons for devices which have no hover functionality. Taken from:
+	https://wiki.selfhtml.org/wiki/Navigation/Dropdown-Men%C3%BC#inklusives_Dropdown-Men.C3.BC
+*/
+document.addEventListener('DOMContentLoaded', function () {
+	dropdownExtension();
+
+	function dropdownExtension() {
+		const submenus = document.querySelectorAll('nav li > ul');
+		for (let submenu of submenus) {
+			submenu.classList.add('submenu');
+			submenu.insertAdjacentHTML('beforebegin',
+				`
+			<button aria-expanded="false">
+				<span class="visually-hidden">Open submenu</span>
+				<img src="/static/img/menu_expand.png">
+			</button>
+		`
+			);
+		}
+		document.documentElement.addEventListener('click', event => {
+			if (event.target.tagName == 'BUTTON' && event.target.hasAttribute(
+					'aria-expanded')) {
+				event.target.setAttribute('aria-expanded', event.target.getAttribute(
+					'aria-expanded') != 'true');
+				event.target.nextElementSibling.classList.toggle('visible');
+			}
+		});
+		document.addEventListener('keyup', (event) => {
+			if (event.key === 'Escape') {
+				hideSubmenu();
+			}
+			if ((event.key === 'Tab') && (!event.target.closest('.visible'))) {
+				hideSubmenu();
+			}
+		});
+
+		function hideSubmenu() {
+			let elements = document.querySelectorAll('.visible');
+			elements.forEach(function (element) {
+				element.classList.remove('visible');
+			});
+			let buttons = document.querySelectorAll('[aria-expanded="true"]');
+			buttons.forEach(function (button) {
+				button.setAttribute('aria-expanded', 'false');
+			});
+		}
+	}
+});
+
 /*
 function toggleHidden(cl) {
   var x = document.getElementsByClassName(cl);
