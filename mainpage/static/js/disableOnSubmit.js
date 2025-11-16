fdcc7fc70913089edcf1b702c17c8174f1098610
frankystone <>
2025-11-16 16:48:44 +0100

    removed text/javascript and other js fixes

--- mainpage/static/js/disableOnSubmit.js
+++ mainpage/static/js/disableOnSubmit.js
@@ -2,16 +2,16 @@
  * jQuery Disable On Submit Plugin
  * http://www.evanbot.com/article/jquery-disable-on-submit-plugin/13
  *
- * Copyright (c) 2009 Evan Byrne (http://www.evanbot.com)     
+ * Copyright (c) 2009 Evan Byrne (http://www.evanbot.com)
  */
 $.fn.disableOnSubmit = function(disableList){
-	
-	if(disableList === null){var $list = 'input[type=submit],input[type=button],input[type=reset],button';}
+
+	if(disableList === undefined){var $list = "input[type='submit'],input[type='button'],input[type='reset'],button";}
 	else{var $list = disableList;}
-	
+
 	// Makes sure button is enabled at start
-	$(this).find($list).prop("disabled", true);
-	
+	$(this).find($list).prop("disabled", false);
+
 	$(this).submit(function(){$(this).find($list).attr('disabled','disabled');});
 	return this;
 };
