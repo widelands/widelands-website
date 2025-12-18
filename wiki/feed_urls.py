2c2ed5dfa15dffeea8c4667e82edea8f7f45edcc
frankystone <>
2025-12-18 12:41:35 +0100

    addressed review

--- wiki/feed_urls.py
+++ wiki/feed_urls.py
@@ -9,15 +9,15 @@ from wiki.feeds import (
 )
 
 urlpatterns = [
-    re_path(r"^feeds/rss/$", RssHistoryFeed(), name="wiki_history_feed_rss"),
-    re_path(r"^feeds/atom/$", AtomHistoryFeed(), name="wiki_history_feed_atom"),
+    re_path(r"^rss/$", RssHistoryFeed(), name="wiki_history_feed_rss"),
+    re_path(r"^atom/$", AtomHistoryFeed(), name="wiki_history_feed_atom"),
     re_path(
-        r"^(?P<title>" + settings.WIKI_URL_RE + r")/feeds/rss/$",
+        r"^(?P<title>" + settings.WIKI_URL_RE + r")/rss/$",
         RssArticleHistoryFeed(),
         name="wiki_article_history_feed_rss",
     ),
     re_path(
-        r"^(?P<title>" + settings.WIKI_URL_RE + r")/feeds/atom/$",
+        r"^(?P<title>" + settings.WIKI_URL_RE + r")/atom/$",
         AtomArticleHistoryFeed(),
         name="wiki_article_history_feed_atom",
     ),
